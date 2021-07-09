ca6b69ccee60a5ef4a67e4c59394fcf861778213
Samily Lima <66699126+SamilyLima@users.noreply.github.com>
2021-07-09 19:14:23 -0400

    Add files via upload

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -0,0 +1,35 @@
+package com.rodrigopeleias.bookstoremanager.controller;
+
+
+import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
+import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
+import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.exception.BookNotFoundException;
+import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
+import com.rodrigopeleias.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
+       return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id) throws BookNotFoundException {
+        return bookService.finById(id);
+    }
+}
---
+++ src/test/java/com/rodrigopeleias/bookstoremanager/controller/BookControllerTest.java
@@ -0,0 +1,79 @@
+package com.rodrigopeleias.bookstoremanager.controller;
+
+import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
+import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
+import com.rodrigopeleias.bookstoremanager.service.BookService;
+import com.rodrigopeleias.bookstoremanager.utils.BookUtils;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static com.rodrigopeleias.bookstoremanager.utils.BookUtils.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BookService bookService;
+
+    @InjectMocks
+    private BookController bookController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(bookController)
+        .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+        .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+        .build();
+
+    }
+
+    @Test
+    void testWhenPOSTisCalledThenABookShouldBeCreated() throws Exception {
+
+        BookDTO bookDTO = createFakeBookDTO();
+        MessageResponseDTO expectedMessageResponse = MessageResponseDTO.builder()
+                .message("Book created with ID " + bookDTO.getId())
+                .build();
+
+        when(bookService.create(bookDTO)).thenReturn(expectedMessageResponse);
+
+        mockMvc.perform(post("/api/v1/books")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(asJsonString(bookDTO)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.message", Is.is(expectedMessageResponse.getMessage())));
+    }
+
+
+    @Test
+    void testWhenPOSTwithInvalidISBNIsCalledThenBadRequestShouldBeReturned() throws Exception {
+
+        BookDTO bookDTO = createFakeBookDTO();
+        bookDTO.setIsbn("invalid isbn");
+
+
+        mockMvc.perform(post("/api/v1/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(bookDTO)))
+                .andExpect(status().isBadRequest());
+    }
+}
